0a47fcfae78745bf9c3aecc8218cc44a8cab90fe
AshNicolus <yashnema52@gmail.com>
2025-05-25 12:07:12 +0530

    Implementing Retrieval Enging

---
+++ retrieval-engine/src/__init__.py
@@ -0,0 +1,13 @@
+"""
+Retrieval & Query Engine Component
+
+This component handles:
+1. Hybrid search implementation
+2. Query routing
+3. Context-aware retrieval
+4. Ranking and relevance scoring
+"""
+
+from typing import Dict, List, Optional, Union
+
+ 
